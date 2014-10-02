65e9d14ac38cd1e11c0576f8af193720f5cd8ea5
Gerardo Grimaldi <Grimaldi.gerardo@gmail.com>
2014-10-02 00:48:32 -0300

    corregido servicio

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,39 +1,16 @@
-var app = angular.module('dolarHoy2.controllers', []);
-
-app.controller('DolarCtrl', function DolarCtrl($scope, dolarFactory) {
-  $scope.dolar = dolarFactory;
-});
-
-app.service('dolarService', function() {
-  var that = this;
-    this.dolar = {};
-
-    this.initDolar = function() {
-      $http.get('http://dolarhoyserver.herokuapp.com/dolar/Hola123!')
-      .success(function(data) {
-        that.dolar = data.dolar;
-      })
-      .error(function(data) {
-        console.log('Error: ' + data.message);
+angular.module('dolarHoy2.controllers', [])
+  .controller('DolarCtrl', function DolarCtrl($scope, $rootScope, $ionicLoading, dolarService) {
+    var scope = $rootScope;
+    $scope.load = function() {
+      $ionicLoading.show({
+        template: 'Actualizando datos...'
       });
-    };
-
-    this.getDolar = function() {
-      return this.dolar;
-    };
-});
-
-app.factory('dolarFactory', function($http) {
-
-  return JSON.parse('{"_id":"541c552d83247a020000002c","dolarCompra":"8.390","dolarVenta":"8.440","dolarBlueCompra":"14.900","dolarBlueVenta":"14.970","dolarTarjeta":"11.394","realCompra":"2.3757","realVenta":"2.3762","euroCompra":"10.900","euroVenta":"11.350","date":"2014-09-19T13:09:17.000Z"}');
-    // $http.get('http://dolarhoyserver.herokuapp.com/dolar/Hola123!')
-    // .success(function(data) {
-    //   debugger;
-    //   //return JSON.parse(data);
-    //
-    // })
-    // .error(function(data) {
-    //   console.log('Error: ' + data.message);
-    // });
-
-});
+      dolarService.getData().then(function(data) {
+        $scope.dolar = data;
+        $scope.hide();
+      });
+    }
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
+  });
--- www/js/services/services.js
+++ www/js/services/services.js
@@ -1,3 +1,47 @@
-//
-// angular.module('dolarHoy2.controllers', [])
-//
+angular.module('dolarHoy2.controllers')
+  .service('dolarServiceOld', function() {
+
+    var that = this;
+    this.dolar = {};
+
+    this.initDolar = function() {
+      $http.get('http://dolarhoyserver.herokuapp.com/dolar/Hola123!')
+      .success(function(data) {
+        that.dolar = data.dolar;
+      })
+      .error(function(data) {
+        console.log('Error: ' + data.message);
+      });
+    };
+
+    this.getDolar = function() {
+      return this.dolar;
+    };
+  });
+
+angular.module('dolarHoy2.controllers')
+  .factory('dolarFactory', function($http) {
+
+  return //JSON.parse('{"_id":"541c552d83247a020000002c","dolarCompra":"8.390","dolarVenta":"8.440","dolarBlueCompra":"14.900","dolarBlueVenta":"14.970","dolarTarjeta":"11.394","realCompra":"2.3757","realVenta":"2.3762","euroCompra":"10.900","euroVenta":"11.350","date":"2014-09-19T13:09:17.000Z"}');
+     $http.get('http://dolarhoyserver.herokuapp.com/dolar/Hola123!')
+     .success(function(data) {
+       debugger;
+       return JSON.parse(data);
+
+     })
+     .error(function(data) {
+       console.log('Error: ' + data.message);
+     });
+
+});
+
+angular.module('dolarHoy2.controllers')
+  .service('dolarService', function($q, $compile, $http) {
+    this.getData = function() {
+      var promise = $http.get('http://dolarhoyserver.herokuapp.com/dolar/Hola123!');
+      promise = promise.then(function (response) {
+        return response.data;
+      });
+      return promise;
+    };
+  });
