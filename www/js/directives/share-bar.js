3e0d15e37c33d2cbe08c06e5b4ec91998368f713
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2015-11-12 18:09:30 -0300

    change

--- www/js/directives/share-bar.js
+++ www/js/directives/share-bar.js
@@ -14,7 +14,7 @@ angular
         templateUrl: 'templates/directives/shareBar.html',
         link: function (scope, element, attrs) {
 
-          var message = function() {
+          var makeMessage = function() {
             return (!unico) ? 'Los valores de hoy del ' + type + ' son ' + 'Dolar compra ' + scope.compra + ' Dolar' +
             ' venta ' + scope.venta : 'El valor del dolar tarjeta es ' + scope.unico
           };
@@ -24,7 +24,7 @@ angular
               template: '<p>Compartiendo...</p><ion-spinner></ion-spinner>'
             });
             $cordovaSocialSharing
-                .shareViaFacebook(message(), image, link)
+                .shareViaFacebook(makeMessage(), image, link)
                 .then(function (result) {
                   $ionicLoading.hide();
                 }, function (err) {
@@ -36,7 +36,7 @@ angular
               template: '<p>Compartiendo...</p><ion-spinner></ion-spinner>'
             });
             $cordovaSocialSharing
-                .shareViaTwitter(message(), image, link)
+                .shareViaTwitter(makeMessage(), image, link)
                 .then(function (result) {
                   $ionicLoading.hide();
                 }, function (err) {
@@ -49,7 +49,7 @@ angular
               template: '<p>Compartiendo...</p><ion-spinner></ion-spinner>'
             });
             $cordovaSocialSharing
-                .shareViaWhatsApp(message(), image, link)
+                .shareViaWhatsApp(makeMessage(), image, link)
                 .then(function (result) {
                   $ionicLoading.hide();
                 }, function (err) {
